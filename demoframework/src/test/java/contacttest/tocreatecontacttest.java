a345421478b717ff11c90e35c02cc56f75e339ba
DELL <DELL@DESKTOP-107SQ0V>
2024-10-03 14:23:09 +0530

    First Push

---
+++ demoframework/src/test/java/contacttest/tocreatecontacttest.java
@@ -0,0 +1,10 @@
+package contacttest;
+
+public class tocreatecontacttest {
+
+	public static void main(String[] args) {
+		System.out.println("contact created succesfully");
+
+	}
+
+}
---
+++ demoframework/src/test/java/organizationtest/tocreateorganizationtest.java
@@ -0,0 +1,10 @@
+package organizationtest;
+
+public class tocreateorganizationtest {
+
+	public static void main(String[] args) {
+	 System.out.println("organization created successfully");
+
+	}
+
+}
